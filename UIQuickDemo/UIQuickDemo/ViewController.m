c72227c37250fad7a012802f37374d81c9b6059e
蔡联庆 <cailianqing@didichuxing.coom>
2019-09-24 16:40:23 +0800

    优化

--- UIQuickDemo/UIQuickDemo/ViewController.m
+++ UIQuickDemo/UIQuickDemo/ViewController.m
@@ -19,37 +19,37 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
 
-    NSString *testStr = @"据新华社消息，记者从庆祝新中国成立70周年庆萨达";
-    
+    NSString *testStr = @"是两地分居手里的荆防颗粒时代峻峰莱胜收到了非技术砥砺奋进乐山大佛技术大佬开发加手里的,收到了废旧塑料看点风景收到了开发叫啥浪蝶狂蜂加速度快了房价手里的空间放螺丝孔";
     UILabel *a = [[UILabel alloc]init];
     a.numberOfLines = 2;
+    a.lineBreakMode = NSLineBreakByCharWrapping;
     [a sizeToFit];
-    a.frame = CGRectMake(100, 100,200, 80);
+    a.frame = CGRectMake(100, 100,200, 100);
     a.text = testStr;
     [self.view addSubview:a];
     
     NSArray *b = [self getSeparatedLinesFromLabel:a];
-    if (b.count == 2) {
-        NSString *first = b.firstObject;
-        NSString *last = b.lastObject;
-        if (first.length < last.length
-            && (last.length - first.length)>=3) {
-            NSString *subStr = [testStr substringFromIndex:3];
-            NSString *tempStr = [@"..." stringByAppendingString:subStr];
-            a.text = tempStr;
-        }
-    }
+//    if (b.count == 2) {
+//        NSString *first = b.firstObject;
+//        NSString *last = b.lastObject;
+//        if (first.length < last.length
+//            && (last.length - first.length)>=3) {
+//            NSString *subStr = [testStr substringFromIndex:2];
+//            NSString *tempStr = [@"..." stringByAppendingString:subStr];
+//            a.text = tempStr;
+//        }
+//    }
     
     if (b.count >= 3) {
         NSString *first = b.firstObject;
         NSString *third = [b objectAtIndex:2];
         if (third.length <= 3) {
-            NSString *subStr = [testStr substringFromIndex:3];
+            NSString *subStr = [testStr substringFromIndex:1];
             NSString *tempStr = [@"..." stringByAppendingString:subStr];
             a.text = tempStr;
         }else {
             NSString *subStr = [testStr substringFromIndex:(testStr.length - first.length*2)];
-            NSString *subStr1 = [subStr substringFromIndex:3];
+            NSString *subStr1 = [subStr substringFromIndex:1];
             NSString *tempStr = [@"..." stringByAppendingString:subStr1];
             a.text = tempStr;
         }
@@ -66,7 +66,7 @@
     [attStr addAttribute:(NSString *)kCTFontAttributeName value:(__bridge id)myFont range:NSMakeRange(0, attStr.length)];
     CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((__bridge CFAttributedStringRef)attStr);
     CGMutablePathRef path = CGPathCreateMutable();
-    CGPathAddRect(path, NULL, CGRectMake(0,0,rect.size.width,100000));
+    CGPathAddRect(path, NULL, CGRectMake(0,0,rect.size.width,MAXFLOAT));
     CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, 0), path, NULL);
     NSArray *lines = (__bridge NSArray *)CTFrameGetLines(frame);
     NSMutableArray *linesArray = [[NSMutableArray alloc]init];
